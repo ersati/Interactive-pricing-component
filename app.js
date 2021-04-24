732a7af402013623469349ccbaa18d03bad1d265
ersati <piotrp92@gmail.com>
2021-04-24 10:48:07 +0100

    add features where tracks has 2 colors left side is green right grey

--- app.js
+++ app.js
@@ -23,4 +23,18 @@ updateValue()
 
 inputRange.addEventListener('input', updateValue)
 
-inputSwitch.addEventListener('input', updateValue)
+inputSwitch.addEventListener('input', updateValue);
+
+
+const slider = document.querySelector('input[type="range"] ');
+
+
+const min = slider.min
+const max = slider.max
+const value = slider.value
+
+slider.style.background = `linear-gradient(to right, var(--softCyan) 0%, var(--softCyan) ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`
+
+slider.oninput = function() {
+  this.style.background = `linear-gradient(to right, var(--softCyan) 0%, var(--softCyan) ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
+};
